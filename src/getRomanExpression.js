4573382b447789290ba54dbe5916958d8585d292
Daniel Maldonado <danielmaldonado@Daniels-MacBook-Air.local>
2019-06-12 12:19:27 -0300

    added another impl after all test passing

--- src/getRomanExpression.js
+++ src/getRomanExpression.js
@@ -1,4 +1,4 @@
-function getRomanExpression(n) {
+function FIRST_IMPL_TDD_getRomanExpression(n) {
     if (n >= 40) {
         return "XL" + getRomanExpression(n - 40);
     }
@@ -15,6 +15,27 @@ function getRomanExpression(n) {
         return "IV";
     }
     return "I".repeat(n);
+}
+//function refactorized after all test passing.
+function getRomanExpression(
+    n,
+    categories = [[40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]], 
+    acc = "",
+) {
+    const [categoryIndex, categoryName] = categories[0];
+    return n === 0
+        ? acc
+        : n >= categoryIndex
+            ? getRomanExpression(
+                n - categoryIndex,
+                categories,
+                acc + categoryName,
+            )
+            : getRomanExpression(
+                n,
+                categories.filter(([otherCategory]) => otherCategory !== categoryIndex),
+                acc,
+            )
 }
 
 module.exports = getRomanExpression;
